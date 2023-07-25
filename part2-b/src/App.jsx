9296feed21ea2d2691869fb1340f28e5483c8f28
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-07-25 11:40:44 +0545

    feat: added form and prevent default rendering

--- part2-b/src/App.jsx
+++ part2-b/src/App.jsx
@@ -1,8 +1,27 @@
-const App = () => {
+import { useState } from "react";
+import Note from "./components/Note";
+
+const App = (props) => {
+  console.log("App component rendered", props);
+  const [notes, setNotes] = useState(props.notes);
+  const addNote = (e) => {
+    e.preventDefault();
+    console.log("Form submitted", e.target);
+  };
   return (
-    <>
-      <h1>Rendering from App.jsx using vite.</h1>
-    </>
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        <ul>
+          {notes.map((note) => (
+            <Note key={note.id} note={note} />
+          ))}
+        </ul>
+      </ul>
+      <form onSubmit={addNote}>
+        <button type="submit">click me</button>
+      </form>
+    </div>
   );
 };
 
