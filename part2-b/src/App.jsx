b68cac2f5dafad9710606088fde20c9aaf063034
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-07-25 12:03:33 +0545

    feat: working note app

--- part2-b/src/App.jsx
+++ part2-b/src/App.jsx
@@ -4,10 +4,29 @@ import Note from "./components/Note";
 const App = (props) => {
   console.log("App component rendered", props);
   const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+
   const addNote = (e) => {
     e.preventDefault();
     console.log("Form submitted", e.target);
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    if (newNote) {
+      setNotes(notes.concat(noteObject));
+    }
+
+    setNewNote("");
   };
+
+  const handleChange = (e) => {
+    console.log("from handleChange fun", e.target.value);
+    setNewNote(e.target.value);
+  };
+
   return (
     <div>
       <h1>Notes</h1>
@@ -19,6 +38,7 @@ const App = (props) => {
         </ul>
       </ul>
       <form onSubmit={addNote}>
+        <input type="text" value={newNote} onChange={handleChange} />
         <button type="submit">click me</button>
       </form>
     </div>
