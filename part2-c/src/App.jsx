a014d7b25f459dddead7e42e1cf39091ea667275
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-07-27 04:38:44 +0545

    feat: added get and post request; need to refactor post request code

--- part2-c/src/App.jsx
+++ part2-c/src/App.jsx
@@ -1,8 +1,53 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Note from "./components/Note";
+
 const App = () => {
+  // console.log("App component rendered", props);
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/notes").then((res) => {
+      console.log(res);
+      setNotes(res.data);
+    });
+  }, []);
+
+  const addNote = (e) => {
+    e.preventDefault();
+    // console.log("Form submitted", e.target);
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    };
+    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+      // console.log(response);
+      setNotes(notes.concat(response.data));
+      setNewNote("");
+    });
+  };
+
+  const handleChange = (e) => {
+    console.log("from handleChange fun", e.target.value);
+    setNewNote(e.target.value);
+  };
+
   return (
-    <>
-      <h1>Rendering from App.jsx using vite.</h1>
-    </>
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        <ul>
+          {notes.map((note) => (
+            <Note key={note.id} note={note} />
+          ))}
+        </ul>
+      </ul>
+      <form onSubmit={addNote}>
+        <input type="text" value={newNote} onChange={handleChange} />
+        <button type="submit">click me</button>
+      </form>
+    </div>
   );
 };
 
