f0a3ea144e689df673cda72951ceb9d8d14dfeef
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-08-18 14:17:21 +0545

    added testing code

---
+++ part4-a/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: { node: true, commonjs: true, es2021: true, jest: true },
+  extends: "eslint:recommended",
+  overrides: [
+    {
+      env: { node: true },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: { sourceType: "script" },
+    },
+  ],
+  parserOptions: { ecmaVersion: "latest" },
+  rules: { eqeqeq: "error", "no-console": 0 },
+};
