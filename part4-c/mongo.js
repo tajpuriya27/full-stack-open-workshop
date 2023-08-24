b544ea819093be7d877971c02ea9fcea87d8b087
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-08-24 10:25:27 +0545

    chore: copied part4-b to part4-c dir

---
+++ part4-c/mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://tajpuriya27:${password}@cluster0.kedtqza.mongodb.net/workshoppart3?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "Note created from mongo.js file",
+  important: true,
+});
+
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
+
+// Note.find({}).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
---
+++ part4-c/tests/test_helper.js
@@ -0,0 +1,31 @@
+const Note = require("../models/note");
+
+const initialNotes = [
+  {
+    content: "Notes created for testing purpose 00",
+    important: false,
+  },
+  {
+    content: "Notes created for testing purpose 01",
+    important: true,
+  },
+];
+
+const nonExistingId = async () => {
+  const note = new Note({ content: "willremovethissoon" });
+  await note.save();
+  await note.deleteOne();
+
+  return note._id.toString();
+};
+
+const notesInDb = async () => {
+  const notes = await Note.find({});
+  return notes.map((note) => note.toJSON());
+};
+
+module.exports = {
+  initialNotes,
+  nonExistingId,
+  notesInDb,
+};
