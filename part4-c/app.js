2fefdebc77c823ae2111b87302b82522a27ab9c1
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-08-25 12:48:16 +0545

    feat: user-schema and post request functionality added

--- part4-c/app.js
+++ part4-c/app.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const notesRouter = require("./controllers/notes");
 const middleware = require("./utils/middleware");
 const mongoose = require("mongoose");
+const usersRouter = require("./controllers/users");
 
 mongoose.set("strictQuery", false);
 
@@ -29,6 +30,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
