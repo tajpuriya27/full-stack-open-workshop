caf4d274a62179c85ff2fcdd88f829686b66d4b8
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-08-26 00:53:45 +0545

    feat: time validation on token added

--- part4-d/controllers/login.js
+++ part4-d/controllers/login.js
@@ -21,7 +21,12 @@ loginRouter.post("/", async (request, response) => {
     id: user._id,
   };
 
-  const token = jwt.sign(userForToken, process.env.SECRET);
+  //   const token = jwt.sign(userForToken, process.env.SECRET);
+
+  // token expires in 60*1 seconds, that is, in one minute
+  const token = jwt.sign(userForToken, process.env.SECRET, {
+    expiresIn: 60 * 1,
+  });
 
   response
     .status(200)
