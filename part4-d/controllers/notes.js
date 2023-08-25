79601600981e216889fece2b9d493dd707d38d3b
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-08-26 00:45:23 +0545

    feat: added token functionality

--- part4-d/controllers/notes.js
+++ part4-d/controllers/notes.js
@@ -1,6 +1,15 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 notesRouter.get("/", async (request, response) => {
   const notes = await Note.find({}).populate("user", { username: 1 });
@@ -19,7 +28,13 @@ notesRouter.get("/:id", async (request, response) => {
 notesRouter.post("/", async (request, response, next) => {
   const body = request.body;
 
-  const user = await User.findById(body.userId);
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+
+  // const user = await User.findById(body.userId);
 
   const note = new Note({
     content: body.content,
