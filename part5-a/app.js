57a9b5f774abc78e05e3566974ce491e6cd30e32
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-08-26 15:41:02 +0545

    chore: copied part4-d to part5-a

---
+++ part5-a/app.js
@@ -0,0 +1,40 @@
+const config = require("./utils/config");
+const express = require("express");
+require("express-async-errors");
+const app = express();
+const cors = require("cors");
+const notesRouter = require("./controllers/notes");
+const middleware = require("./utils/middleware");
+const mongoose = require("mongoose");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+
+mongoose.set("strictQuery", false);
+
+console.log("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/login", loginRouter);
+app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
