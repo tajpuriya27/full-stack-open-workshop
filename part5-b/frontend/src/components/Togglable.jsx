907969cfc108d86531dac3df031fe1acd9c9a23d
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-06 07:23:29 +0545

    5b: prop-types

--- part5-b/frontend/src/components/Togglable.jsx
+++ part5-b/frontend/src/components/Togglable.jsx
@@ -1,4 +1,5 @@
 import { useState, forwardRef, useImperativeHandle } from "react";
+import PropTypes from "prop-types";
 
 const Togglable = forwardRef((props, refs) => {
   console.log("props from togglable:", props);
@@ -29,5 +30,11 @@ const Togglable = forwardRef((props, refs) => {
     </div>
   );
 });
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
+
+Togglable.displayName = "Togglable";
 
 export default Togglable;
