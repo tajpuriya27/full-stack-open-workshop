a25fb2637a0f346c0940384cd772ebfd2781431d
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-07 16:38:24 +0545

    feat: 6a.4.1 noteApp stable version

--- part6/noteApp/src/index.jsx
+++ part6/noteApp/src/index.jsx
@@ -1,5 +1,71 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import { createStore } from "redux";
 
-createRoot(document.getElementById("root")).render(<App />);
+const noteReducer = (state = [], action) => {
+  switch (action.type) {
+    case "NEW_NOTE": {
+      return state.concat(action.payload);
+    }
+    case "TOGGLE_IMPORTANCE": {
+      console.log(state);
+
+      return state.map((note) =>
+        note.id === action.payload.id
+          ? { ...note, important: !note.important }
+          : note
+      );
+    }
+    default:
+      return state;
+  }
+};
+
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: "NEW_NOTE",
+  payload: {
+    content: "the app state is in redux store",
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: "NEW_NOTE",
+  payload: {
+    content: "state changes are made with actions",
+    important: false,
+    id: 2,
+  },
+});
+
+const toggleImportanceOf = (id) => {
+  store.dispatch({
+    type: "TOGGLE_IMPORTANCE",
+    payload: {
+      id,
+    },
+  });
+};
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id} onClick={() => toggleImportanceOf(note.id)}>
+            {note.content} <strong>{note.important ? "important" : ""}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
+store.subscribe(() => {
+  root.render(<App storeToSubscribe={store} />);
+});
