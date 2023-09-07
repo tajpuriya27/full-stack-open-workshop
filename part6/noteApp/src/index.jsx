19a81b605cd285fa0374207d04a512c4b78823dd
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-07 18:20:40 +0545

    feat: 6a.5 uncontrolled form

--- part6/noteApp/src/index.jsx
+++ part6/noteApp/src/index.jsx
@@ -41,18 +41,36 @@ store.dispatch({
   },
 });
 
-const toggleImportanceOf = (id) => {
-  store.dispatch({
-    type: "TOGGLE_IMPORTANCE",
-    payload: {
-      id,
-    },
-  });
-};
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    store.dispatch({
+      type: "NEW_NOTE",
+      payload: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    });
+  };
+
+  const toggleImportanceOf = (id) => {
+    store.dispatch({
+      type: "TOGGLE_IMPORTANCE",
+      payload: { id },
+    });
+  };
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
         {store.getState().map((note) => (
           <li key={note.id} onClick={() => toggleImportanceOf(note.id)}>
