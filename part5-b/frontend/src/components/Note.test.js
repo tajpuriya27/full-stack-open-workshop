80bd8badc61852f8c657aeb8e4d8e3e8751f3f04
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-08 06:40:06 +0545

    feat: 5c.2 'upto clicking button in tests'

--- part5-b/frontend/src/components/Note.test.js
+++ part5-b/frontend/src/components/Note.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Note from "./Note";
 
 test("renders content", () => {
@@ -9,10 +10,36 @@ test("renders content", () => {
     important: true,
   };
 
-  render(<Note note={note} />);
+  // render(<Note note={note} />);
+  // screen.debug();
 
-  const element = screen.getByText(
+  // const element = screen.getByText(
+  //   "Component testing is done with react-testing-library"
+  // );
+  // expect(element).toBeDefined();
+
+  const { container } = render(<Note note={note} />);
+
+  const div = container.querySelector(".note");
+  screen.debug(div);
+  expect(div).toHaveTextContent(
     "Component testing is done with react-testing-library"
   );
-  expect(element).toBeDefined();
+});
+
+test("clicking the button calls event handler once", async () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true,
+  };
+
+  const mockHandler = jest.fn();
+
+  render(<Note note={note} toggleImportance={mockHandler} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText("make not important");
+  await user.click(button);
+
+  expect(mockHandler.mock.calls).toHaveLength(1);
 });
