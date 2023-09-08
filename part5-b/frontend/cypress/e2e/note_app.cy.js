84ae5acf8eeff8a83047e29ce95ad552447896d5
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-08 21:14:28 +0545

    feat: 5d.1 'writing to form'

--- part5-b/frontend/cypress/e2e/note_app.cy.js
+++ part5-b/frontend/cypress/e2e/note_app.cy.js
@@ -1,7 +1,23 @@
 describe("Note app", function () {
-  it("front page can be opened", function () {
+  beforeEach(function () {
     cy.visit("http://localhost:5173");
+  });
+
+  it("front page can be opened", function () {
     cy.contains("Notes");
     cy.contains("Testing from Nepal");
+  });
+
+  it("login form can be opened", function () {
+    cy.contains("Click to login").click();
+  });
+
+  it("user can login", function () {
+    cy.contains("Click to login").click();
+    cy.get("#username").type("tajpuriya");
+    cy.get("#password").type("tajpuriya27");
+    cy.get("#login-button").click();
+
+    cy.contains("Sunil Tajpuriya logged in");
   });
 });
