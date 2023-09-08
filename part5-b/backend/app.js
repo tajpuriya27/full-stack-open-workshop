e5accf49dd2bf43cd408ec04871b01d259704803
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-08 21:45:38 +0545

    feat: 5d.3 'controlling the state of dB'

--- part5-b/backend/app.js
+++ part5-b/backend/app.js
@@ -34,6 +34,12 @@ app.use("/api/login", loginRouter);
 app.use("/api/notes", notesRouter);
 app.use("/api/users", usersRouter);
 
+if (process.env.NODE_ENV === "test") {
+  console.log("logging");
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
--- part5-b/frontend/cypress/e2e/note_app.cy.js
+++ part5-b/frontend/cypress/e2e/note_app.cy.js
@@ -1,11 +1,17 @@
 describe("Note app", function () {
   beforeEach(function () {
+    cy.request("POST", "http://localhost:3001/api/testing/reset");
+    const user = {
+      name: "Sunil Tajpuriya",
+      username: "tajpuriya",
+      password: "tajpuriya27",
+    };
+    cy.request("POST", "http://localhost:3001/api/users/", user);
     cy.visit("http://localhost:5173");
   });
 
   it("front page can be opened", function () {
     cy.contains("Notes");
-    cy.contains("Testing from Nepal");
   });
 
   it("login form can be opened", function () {
@@ -34,6 +40,22 @@ describe("Note app", function () {
       cy.get("#note-input").type("a note created by cypress");
       cy.contains("save").click();
       cy.contains("a note created by cypress");
+    });
+
+    describe("and a note exists", function () {
+      beforeEach(function () {
+        cy.contains("Add note").click();
+        cy.get("input").type("another note cypress");
+        cy.contains("save").click();
+      });
+
+      it("it can be made not important", function () {
+        cy.contains("another note cypress")
+          .contains("make not important")
+          .click();
+
+        cy.contains("another note cypress").contains("make important");
+      });
     });
   });
 });
