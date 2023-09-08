842c755bdbb5f2e916ae568ce96a6d2a3dcbfb6f
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-08 11:07:46 +0545

    feat: 5c.5.4 added queryByText

--- part5-b/frontend/src/components/Note.test.js
+++ part5-b/frontend/src/components/Note.test.js
@@ -59,3 +59,15 @@ test("clicking the button calls event handler once", async () => {
 
   expect(mockHandler.mock.calls).toHaveLength(1);
 });
+
+test("does not render this", () => {
+  const note = {
+    content: "This is a reminder",
+    important: true,
+  };
+
+  render(<Note note={note} />);
+
+  const element = screen.queryByText("do not want this thing to be rendered");
+  expect(element).toBeNull();
+});
