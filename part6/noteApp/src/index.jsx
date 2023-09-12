96b3459d3f79d4dd09d03c8640dff1b3543fd3ba
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-12 07:55:13 +0545

    6c.1.0 getting data from backend v1

--- part6/noteApp/src/index.jsx
+++ part6/noteApp/src/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { configureStore } from "@reduxjs/toolkit";
-import noteReducer from "./reducers/noteReducer";
+import noteReducer, { setNotes } from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer";
 import { Provider } from "react-redux";
 import App from "./App";
+import noteService from "./services/notes";
 
 const reducer = {
   note: noteReducer,
@@ -13,6 +14,14 @@ const reducer = {
 console.log(reducer);
 const store = configureStore({ reducer });
 console.log("printing the `state` from index.jsx", store.getState());
+
+// noteService.getAll().then((notes) =>
+//   notes.forEach((note) => {
+//     store.dispatch(appendNote(note));
+//   })
+// );
+
+noteService.getAll().then((notes) => store.dispatch(setNotes(notes)));
 
 const root = createRoot(document.getElementById("root"));
 root.render(
