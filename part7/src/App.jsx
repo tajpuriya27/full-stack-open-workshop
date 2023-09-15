fbba994cc0b0ff220e3c572ca70a8a54245b12b6
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-15 13:07:27 +0545

    feat: 7a.1 Application navigation structure

--- part7/src/App.jsx
+++ part7/src/App.jsx
@@ -1,8 +1,64 @@
+import { useState } from "react";
+
+const Home = () => (
+  <div>
+    {" "}
+    <h2>TKTL notes app</h2>{" "}
+  </div>
+);
+
+const Notes = () => (
+  <div>
+    {" "}
+    <h2>Notes</h2>{" "}
+  </div>
+);
+
+const Users = () => (
+  <div>
+    {" "}
+    <h2>Users</h2>{" "}
+  </div>
+);
+
 const App = () => {
+  const [page, setPage] = useState("home");
+
+  const toPage = (page) => (event) => {
+    event.preventDefault();
+    setPage(page);
+  };
+
+  const content = () => {
+    if (page === "home") {
+      return <Home />;
+    } else if (page === "notes") {
+      return <Notes />;
+    } else if (page === "users") {
+      return <Users />;
+    }
+  };
+
+  const padding = {
+    padding: 5,
+  };
+
   return (
-    <>
-      <h1>Rendering from App.jsx using vite.</h1>
-    </>
+    <div>
+      <div>
+        <a href="" onClick={toPage("home")} style={padding}>
+          home
+        </a>
+        <a href="" onClick={toPage("notes")} style={padding}>
+          notes
+        </a>
+        <a href="" onClick={toPage("users")} style={padding}>
+          users
+        </a>
+      </div>
+
+      {content()}
+    </div>
   );
 };
 
