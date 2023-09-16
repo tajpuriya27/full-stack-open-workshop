41e8fda96f04b47dc85462a5137570d17ff10545
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-16 21:15:36 +0545

    feat: 7.b.2.1 customHook - RightLeftCounter

--- part7-b/src/App.jsx
+++ part7-b/src/App.jsx
@@ -7,33 +7,37 @@ const useCounter = () => {
     setValue(value + 1);
   };
 
-  const decrease = () => {
-    setValue(value - 1);
-  };
-
-  const zero = () => {
-    setValue(0);
-  };
-
   return {
     value,
     increase,
-    decrease,
-    zero,
   };
 };
 
 const App = () => {
-  const counter = useCounter();
+  const left = useCounter();
+  const right = useCounter();
 
   return (
     <div>
-      <div>{counter.value}</div>
-      <button onClick={counter.increase}>plus</button>
-      <button onClick={counter.decrease}>minus</button>
-      <button onClick={counter.zero}>zero</button>
+      {left.value}
+      <button onClick={left.increase}>left</button>
+      <button onClick={right.increase}>right</button>
+      {right.value}
     </div>
   );
 };
+
+// const App = () => {
+//   const counter = useCounter();
+
+//   return (
+//     <div>
+//       <div>{counter.value}</div>
+//       <button onClick={counter.increase}>plus</button>
+//       <button onClick={counter.decrease}>minus</button>
+//       <button onClick={counter.zero}>zero</button>
+//     </div>
+//   );
+// };
 
 export default App;
