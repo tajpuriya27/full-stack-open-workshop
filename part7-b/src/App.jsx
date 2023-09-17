86c264088da243024d692e4b4f5474b78cad014e
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-17 06:55:33 +0545

    feat: 7.b.2.3 customHook for form added

--- part7-b/src/App.jsx
+++ part7-b/src/App.jsx
@@ -1,9 +1,15 @@
-import { useCounter } from "./CutomHook";
+import { useCounter, useField } from "./CutomHook";
 
 const App = () => {
   const counter = useCounter();
   const left = useCounter();
   const right = useCounter();
+
+  // For form component
+  const name = useField("text");
+  console.log("returned by useField customHook: [name]", name);
+  const birthdate = useField("date");
+  const height = useField("number");
 
   return (
     <>
@@ -21,6 +27,31 @@ const App = () => {
         <button onClick={left.increase}>left</button>
         <button onClick={right.increase}>right</button>
         {right.value}
+      </div>
+      <hr />
+      <div>
+        <h2>Custom Hook used in Form</h2>
+        <form>
+          name:
+          <input type={name.type} value={name.value} onChange={name.onChange} />
+          <br />
+          birthdate:
+          <input
+            type={birthdate.type}
+            value={birthdate.value}
+            onChange={birthdate.onChange}
+          />
+          <br />
+          height:
+          <input
+            type={height.type}
+            value={height.value}
+            onChange={height.onChange}
+          />
+        </form>
+        <div>
+          {name.value} || {birthdate.value} || {height.value}
+        </div>
       </div>
       <hr />
     </>
--- part7-b/src/CutomHook.js
+++ part7-b/src/CutomHook.js
@@ -22,3 +22,17 @@ export const useCounter = () => {
     zero,
   };
 };
+
+export const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
