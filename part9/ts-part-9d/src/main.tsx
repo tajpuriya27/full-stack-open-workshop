9db646b924cd2e93ed4d05baa79b72da741896ac
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-10-06 14:32:24 +0545

    feat: 9d.2.1. contracted code for welcome component

--- part9/ts-part-9d/src/main.tsx
+++ part9/ts-part-9d/src/main.tsx
@@ -11,13 +11,17 @@
 
 import ReactDOM from "react-dom/client";
 
-interface WelcomeProps {
-  name: string;
-}
+// interface WelcomeProps {
+//   name: string;
+// }
 
-const Welcome = (props: WelcomeProps): JSX.Element => {
-  return <h1>Hello, {props.name}</h1>;
-};
+// const Welcome = (props: WelcomeProps): JSX.Element => {
+//   return <h1>Hello, {props.name}</h1>;
+// };
+
+const Welcome = ({ name }: { name: string }): JSX.Element => (
+  <h1>Hello, {name}</h1>
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Welcome name="Sarah" />
