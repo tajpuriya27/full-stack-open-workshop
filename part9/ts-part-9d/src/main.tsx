e3c18364eeb1b46d77972eb24c3b36c77f10b4e9
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-10-06 14:28:37 +0545

    feat: 9d.2 React component with typescript

--- part9/ts-part-9d/src/main.tsx
+++ part9/ts-part-9d/src/main.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.tsx";
+// import "./index.css";
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+
+interface WelcomeProps {
+  name: string;
+}
+
+const Welcome = (props: WelcomeProps): JSX.Element => {
+  return <h1>Hello, {props.name}</h1>;
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Welcome name="Sarah" />
 );
